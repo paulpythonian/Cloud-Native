35d4606eee050cb2fbbd393d672b2023c414e013
Sung Bin Hong <paulpythonian@gmail.com>
2018-02-02 19:33:00 -0500

    app.py users

--- app.py
+++ app.py
@@ -4,6 +4,56 @@ import sqlite3
 
 app = Flask(__name__)
 
+
+
+def list_users():
+    conn = sqlite3.connect('mydb.db')
+    print('Opened database successfully \n\n')
+    api_list = {}
+    cursor = conn.execute("SELECT username, email, password, full_name, id from users")
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['email'] = row[1]
+        a_dict['password'] = row[2]
+        a_dict['name'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+    conn.close()
+
+    return jsonify({'user_list': api_list})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/api/v1/info")
 def home_index():
     conn = sqlite3.connect('mydb.db')
@@ -24,6 +74,9 @@ def home_index():
     return jsonify({'api_version': api_list}), 200
 
 
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users()
 
 
 
