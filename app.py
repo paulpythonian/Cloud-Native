3d6c6de29381c215cec6bd4030a3175e543c8365
Sung Bin Hong <paulpythonian@gmail.com>
2018-02-02 20:54:36 -0500

    error handler fix

--- app.py
+++ app.py
@@ -121,8 +121,9 @@ def get_user(user_id):
 
 @app.route('/api/v1/users', methods=['POST'])
 def create_user():
-    if not request.json or not 'username' in request.json or 'email' in request.json or not 'password' in request.json:
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
         abort(400)
+
     user = {
         'username': request.json['username'],
         'email': request.json['email'],
