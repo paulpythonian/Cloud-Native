35e533290e6435f21e414bbbf555482b60dae389
Sung Bin Hong <paulpythonian@gmail.com>
2018-02-05 14:43:37 -0500

    tweet by id

--- flask_test.py
+++ flask_test.py
@@ -1,12 +1,16 @@
 from app import app
 import unittest
 
+
 class FlaskappTests(unittest.TestCase):
     def setUp(self):
+        # creates a test client
         self.app = app.test_client()
+        # propagate the exceptions to the test client
         self.app.testing = True
 
     def test_users_status_code(self):
-        result = self.app.get('/api/v1/user')
+        # sends HTTP GET request to the application
+        result = self.app.get('/api/v1/users')
+        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
-
