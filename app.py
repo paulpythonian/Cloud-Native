2fbe08c532b5665aabbe713c40072fea65c6e42c
Sung Bin Hong <paulpythonian@gmail.com>
2018-02-05 20:22:02 -0500

    mongo added edit

--- app.py
+++ app.py
@@ -240,22 +240,11 @@ def clearsession():
 
 @app.route("/api/v1/info")
 def home_index():
-    conn = sqlite3.connect('mydb.db')
-    print("Opened database successfully");
     api_list = []
-    cursor = conn.execute("SELECT version, links, methods, buildtime  from apirelease")
-
-    for row in cursor:
-        a_dict = {}
-        a_dict['version'] = row[0]
-        a_dict['links'] = row[1]
-        a_dict['methods'] = row[2]
-        a_dict['buildtime'] = row[3]
-        api_list.append(a_dict)
-
-    conn.close()
-
-    return jsonify({'api_version': api_list}), 200
+    db = connection.cloud_native.apirelease
+    for row in db.find():
+        api_list.append(str(row))
+    return jsonify({'api_version': api_list}),200
 
 
 @app.route('/api/v1/users', methods=['GET'])
